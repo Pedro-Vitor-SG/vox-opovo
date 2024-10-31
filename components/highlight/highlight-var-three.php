3a6d1c67e8b1635e643467880ede93c545917f45
michelemedeiros <michelemedeiros@opovodigital.com>
2024-10-31 13:05:12 -0300

    feat: Arquitetura do vox

---
+++ components/highlight/highlight-var-three.php
@@ -0,0 +1,92 @@
+
+<style type="text/css">
+    @import '../../../vox-opovo/includes/assets/sass/components/highlight/var-three/styles.css';
+</style>
+
+
+<section class="container-xxl my-5">
+  <div class="row">
+    <div class="col-12">
+      <article class="mb-4">
+        <header>
+          <span class="topper">Política</span>
+          <a href="#">
+            <h2 class="title__matter mb-3">Após vacinação rápida e lockdown, Ceará registra primeiro dia sem mortes por Covid-19</h2>
+          </a>
+        </header>
+        <div class="highlight__image">
+            <figure class="m-0 w-100">
+                <img src="https://niteroi.rj.gov.br/wp-content/uploads/2023/02/2023-02-27-Inicio-da-vacinacao-com-a-Pfizer-bivalente-Alex-Ramos-11-scaled.jpg" width:1296; height: 410; alt="Vacina" >
+            </figure>
+        </div>
+      </article>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-lg-3 col-sm-3 col-md-3 col-6 mb-3">
+        <article class="separator__top">
+          <span class="topper mb-1">Entretenimento</span>
+          <a href="#">
+            <h6 class="matter-emphasis">Duda Beat anuncia data de lançamento de seu novo álbum, "Te Amo Lá Fora"</h6>
+          </a>
+        </article>
+    </div>
+    <div class="col-lg-3 col-sm-3 col-md-3 col-6 mb-3">
+        <article class="separator__top">
+          <span class="topper mb-1">Entretenimento</span>
+          <a href="#">
+            <h6 class="matter-emphasis">Duda Beat anuncia data de lançamento de seu novo álbum, "Te Amo Lá Fora"</h6>
+          </a>
+        </article>
+    </div>
+    <div class="col-lg-3 col-sm-3 col-md-3 col-6 mb-3">
+        <article class="separator__top">
+          <span class="topper mb-1">Entretenimento</span>
+          <a href="#">
+            <h6 class="matter-emphasis">Duda Beat anuncia data de lançamento de seu novo álbum, "Te Amo Lá Fora"</h6>
+          </a>
+        </article>
+    </div>
+    <div class="col-lg-3 col-sm-3 col-md-3 col-6 mb-3">
+        <article class="separator__top">
+          <span class="topper mb-1">Entretenimento</span>
+          <a href="#">
+            <h6 class="matter-emphasis">Duda Beat anuncia data de lançamento de seu novo álbum, "Te Amo Lá Fora"</h6>
+          </a>
+        </article>
+    </div>
+    <div class="col-lg-3 col-sm-3 col-md-3 col-6 mb-3">
+        <article class="separator__top">
+          <span class="topper mb-1">Entretenimento</span>
+          <a href="#">
+            <h6 class="matter-emphasis">Duda Beat anuncia data de lançamento de seu novo álbum, "Te Amo Lá Fora"</h6>
+          </a>
+        </article>
+    </div>
+    <div class="col-lg-3 col-sm-3 col-md-3 col-6 mb-3">
+        <article class="separator__top">
+          <span class="topper mb-1">Entretenimento</span>
+          <a href="#">
+            <h6 class="matter-emphasis">Duda Beat anuncia data de lançamento de seu novo álbum, "Te Amo Lá Fora"</h6>
+          </a>
+        </article>
+    </div>
+    <div class="col-lg-3 col-sm-3 col-md-3 col-6 mb-3">
+        <article class="separator__top">
+          <span class="topper mb-1">Entretenimento</span>
+          <a href="#">
+            <h6 class="matter-emphasis">Duda Beat anuncia data de lançamento de seu novo álbum, "Te Amo Lá Fora"</h6>
+          </a>
+        </article>
+    </div>
+    <div class="col-lg-3 col-sm-3 col-md-3 col-6 mb-3">
+        <article class="separator__top">
+          <span class="topper mb-1">Entretenimento</span>
+          <a href="#">
+            <h6 class="matter-emphasis">Duda Beat anuncia data de lançamento de seu novo álbum, "Te Amo Lá Fora"</h6>
+          </a>
+        </article>
+    </div>
+  </div>
+</section>
---
+++ pages/home.php
@@ -0,0 +1,12 @@
+<!-- HEADER -->
+<?php require_once('../pages/header.php')?>
+<!-- HEADER -->
+
+<!-- DESTAQUE 3 -->
+<?php require_once('../components/highlight/highlight-var-three.php')?>
+<!-- DESTAQUE 3 -->
+
+<!-- FOOTER -->
+<?php require_once('../pages/footer.php')?>
+<!-- FOOTER -->
+
