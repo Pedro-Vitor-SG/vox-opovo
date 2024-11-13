b49ac15901900b506df70d450b85873fdc2f60b0
Pedro Vitor <pedro.vitor@opovodigital.com>
2024-11-13 15:01:24 -0300

    fix: chamando componente var one

--- pages/home.php
+++ pages/home.php
@@ -2,6 +2,10 @@
 <?php require_once('../pages/header.php')?>
 <!-- HEADER -->
 
+<!-- DESTAQUE 1 -->
+<?php require_once('../components/highlight/highlight-var-one.php')?>
+<!-- DESTAQUE 1 -->
+
 <!-- DESTAQUE 3 -->
 <?php require_once('../components/highlight/highlight-var-three.php')?>
 <!-- DESTAQUE 3 -->
