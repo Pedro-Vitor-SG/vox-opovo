f060e63bdcfd7b9346a3d89bc1b2bfab9f670812
Pedro Vitor <pedro.vitor@opovodigital.com>
2024-11-27 17:55:38 -0300

    refactor: reestruturação variação um

--- components/highlight/highlight-var-one.php
+++ components/highlight/highlight-var-one.php
@@ -1,92 +1,130 @@
-
 <style type="text/css">
-    @import '../../../vox-opovo/includes/assets/sass/components/highlight/var-one/styles.css';
+  @import '../../../vox-opovo/includes/assets/sass/components/highlight/var-one/styles.css';
 </style>
 
 
 <section class="container-xxl my-5">
-  <div class="row">
-    <div class="col-12">
-      <article class="mb-4">
-        <header>
-          <span class="topper">Política</span>
-          <a href="#">
-            <h2 class="title__matter mb-3">Após vacinação rápida e lockdown, Ceará registra primeiro dia sem mortes por Covid-19</h2>
-          </a>
-        </header>
-        <div class="highlight__image">
-            <figure class="m-0 w-100">
-                <img src="https://niteroi.rj.gov.br/wp-content/uploads/2023/02/2023-02-27-Inicio-da-vacinacao-com-a-Pfizer-bivalente-Alex-Ramos-11-scaled.jpg" width:1296; height: 410; alt="Vacina" >
-            </figure>
+  <div class="row ">
+    <div class="col-lg-4 col-md-6 box-bigger">
+      <figure class="m-0">
+        <img style="width: 100%;" src="https://www.opovo.com.br/_midias/jpg/2024/10/18/760x570/1__reportagem_kitesurfe__22-30958162.jpg" alt="Kitesurfe">
+      </figure>
+    </div>
+    <div class="col-lg-4 col-md-6">
+      <span class="topper">Política</span>
+      <a href="#">
+        <h2 class="title__matter text-truncate-4">Entidades públicas denunciam ato de vandalização e LGBTfobia em Sobral</h2>
+      </a>
+      <div class="content__related">
+        <a href="" class="d-flex align-items-center position-relative">
+          <h3 class="ps-3">Entidades públicas denunciam ato de vandalização e LGBTfobia em Sobral</h3>
+        </a>
+        <a href="" class="d-flex align-items-center position-relative">
+          <h3 class="m-0 ps-3">Vice-líder do governo diz que Planalto falhou ao não conseguir maioria em CPI</h3>
+        </a>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-md-12 ">
+      <div class="separator__top separator__md__top separator__lg__none">
+        <div class="row">
+          <div class="col-md-6 col-lg-12">
+            <div class="box__news ">
+              <div class="row">
+                <div class="col-6">
+                  <figure class="m-0">
+                    <img style="width: 100%;" src="https://www.opovo.com.br/_midias/jpg/2024/05/21/760x570/1_1__gate_industrial_do_setor_2_da_zpe_ceara-27063372.jpg" alt="Kitesurfe">
+                  </figure>
+                </div>
+                <div class="col-6">
+                  <span class="topper">Política</span>
+                  <a href="">
+                    <h6 class="matter-emphasis text-truncate-4">Duda Beat anuncia data de lançamento de seu novo álbum, "Te Amo Lá Fora"</h6>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-12 ">
+            <div class="box__news separator__md__none separator__lg__top separator__top">
+              <div class="row">
+                <div class="col-6">
+                  <figure class="m-0">
+                    <img style="width: 100%;" src="https://www.opovo.com.br/_midias/jpg/2024/11/13/760x570/1_mike_tyson_jake_paul-31428157.jpg" alt="Kitesurfe">
+                  </figure>
+                </div>
+                <div class="col-6">
+                  <span class="topper">Política</span>
+                  <a href="">
+                    <h6 class="matter-emphasis text-truncate-4 ">Rival do Fortaleza no Nordestão, Bahia faz treino de apronto no CT do Ceará</h6>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
-      </article>  
+      </div>
     </div>
   </div>
 
-  <div class="row">
-    <div class="col-lg-3 col-sm-3 col-md-3 col-6 mb-3">
-        <article class="separator__top">
-          <span class="topper mb-1">Entretenimento</span>
-          <a href="#">
-            <h6 class="matter-emphasis">Duda Beat anuncia data de lançamento de seu novo álbum, "Te Amo Lá Fora"</h6>
-          </a>
-        </article>
-    </div>
-    <div class="col-lg-3 col-sm-3 col-md-3 col-6 mb-3">
-        <article class="separator__top">
-          <span class="topper mb-1">Entretenimento</span>
-          <a href="#">
-            <h6 class="matter-emphasis">Duda Beat anuncia data de lançamento de seu novo álbum, "Te Amo Lá Fora"</h6>
-          </a>
-        </article>
-    </div>
-    <div class="col-lg-3 col-sm-3 col-md-3 col-6 mb-3">
-        <article class="separator__top">
-          <span class="topper mb-1">Entretenimento</span>
-          <a href="#">
-            <h6 class="matter-emphasis">Duda Beat anuncia data de lançamento de seu novo álbum, "Te Amo Lá Fora"</h6>
-          </a>
-        </article>
-    </div>
-    <div class="col-lg-3 col-sm-3 col-md-3 col-6 mb-3">
-        <article class="separator__top">
-          <span class="topper mb-1">Entretenimento</span>
-          <a href="#">
-            <h6 class="matter-emphasis">Duda Beat anuncia data de lançamento de seu novo álbum, "Te Amo Lá Fora"</h6>
-          </a>
-        </article>
-    </div>
-    <div class="col-lg-3 col-sm-3 col-md-3 col-6 mb-3">
-        <article class="separator__top">
-          <span class="topper mb-1">Entretenimento</span>
-          <a href="#">
-            <h6 class="matter-emphasis">Duda Beat anuncia data de lançamento de seu novo álbum, "Te Amo Lá Fora"</h6>
-          </a>
-        </article>
-    </div>
-    <div class="col-lg-3 col-sm-3 col-md-3 col-6 mb-3">
-        <article class="separator__top">
-          <span class="topper mb-1">Entretenimento</span>
-          <a href="#">
-            <h6 class="matter-emphasis">Duda Beat anuncia data de lançamento de seu novo álbum, "Te Amo Lá Fora"</h6>
-          </a>
-        </article>
-    </div>
-    <div class="col-lg-3 col-sm-3 col-md-3 col-6 mb-3">
-        <article class="separator__top">
-          <span class="topper mb-1">Entretenimento</span>
-          <a href="#">
-            <h6 class="matter-emphasis">Duda Beat anuncia data de lançamento de seu novo álbum, "Te Amo Lá Fora"</h6>
-          </a>
-        </article>
-    </div>
-    <div class="col-lg-3 col-sm-3 col-md-3 col-6 mb-3">
-        <article class="separator__top">
-          <span class="topper mb-1">Entretenimento</span>
-          <a href="#">
-            <h6 class="matter-emphasis">Duda Beat anuncia data de lançamento de seu novo álbum, "Te Amo Lá Fora"</h6>
-          </a>
-        </article>
+  <div class="separator__lg__top">
+    <div class="row ">
+      <div class="col-lg-4 col-md-6">
+        <div class="box__news separator__top separator__lg__none">
+          <div class="row">
+            <div class="col-6">
+              <figure class="m-0">
+                <img style="width: 100%;" src="https://www.opovo.com.br/_midias/jpg/2024/11/13/760x570/1_mike_tyson_jake_paul-31428157.jpg" alt="Kitesurfe">
+              </figure>
+            </div>
+            <div class="col-6">
+              <span class="topper">Política</span>
+              <a href="">
+                <h6 class="matter-emphasis text-truncate-4">Rival do Fortaleza no Nordestão, Bahia faz treino de apronto no CT do Ceará</h6>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-6">
+        <div class="box__news separator__top separator__lg__none">
+          <div class="row">
+            <div class="col-6">
+              <figure class="m-0">
+                <img style="width: 100%;" src="https://www.opovo.com.br/_midias/jpg/2024/11/13/760x570/1_mike_tyson_jake_paul-31428157.jpg" alt="Kitesurfe">
+              </figure>
+            </div>
+            <div class="col-6">
+              <span class="topper">Política</span>
+              <a href="">
+                <h6 class="matter-emphasis text-truncate-4">Rival do Fortaleza no Nordestão, Bahia faz treino de apronto no CT do Ceará</h6>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 d-md-none d-lg-block">
+        <div class="box__news separator__top separator__lg__none">
+          <div class="row">
+            <div class="col-6">
+              <figure class="m-0">
+                <img style="width: 100%;" src="https://www.opovo.com.br/_midias/jpg/2024/11/13/760x570/1_mike_tyson_jake_paul-31428157.jpg" alt="Kitesurfe">
+              </figure>
+            </div>
+            <div class="col-6">
+              <span class="topper">Política</span>
+              <a href="">
+                <h6 class="matter-emphasis text-truncate-4">Rival do Fortaleza no Nordestão, Bahia faz treino de apronto no CT do Ceará</h6>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
+
+  </div>
+
 </section>
